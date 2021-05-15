fe512d65a778b75ea50bba667aff5d64a945116a
Olivia Owen <olivia.owen@hotmail.com>
2021-05-15 11:58:12 +0100

    Added bootstrap

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import React, { useState } from 'react';
+
 function Navbar() {
     const [isNavCollapsed, setIsNavCollapsed] = useState(true);
     const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
