19662b4560f86fdd5dadec43a0710d07be8fe76d
Olivia Owen <olivia.owen@hotmail.com>
2021-05-16 14:56:45 +0100

    Completed about me section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,18 +3,38 @@ import React, { useState } from 'react';
 function About() {
     return (
         // <!-- This is the title and introduction of the page -->
-        <div>
-            <div class="jumbotron">
-                <h1 class="display-4">Welcome to my portfolio!</h1>
-                <p class="lead">This is my portfolio in which you will find information about me alongside
-                information
-                        and examples of my work.</p>
-
-                {/* <!-- This is the line across the page --> */}
-                <hr class="my-4" />
+            <div className="row">
+                <div className="col-md-8 offset-md-2 bodyPad">
+                    <div id="about-me"></div>
+                    <h2> About me </h2>
                 </div>
+                <div className="col-md-2 offset-md-2 bodyPad">
+                    <img src="./assets/20201213_122059.jpg" alt="Image of me" class="img-thumbnail" /> 
+                </div>
+                <div className="col-md-6 bodyPad">
+                    <p className="lead">
+                        My name is Olivia Owen and i currently work as a Receptionist as well as studying to become a
+                        web
+                        developer with the University of Birmingham Coding Bootcamp.
+                        I currently work at the University and love my job. I would love to become a full-stack
+                        developer after graduating.
+                <br />
+                            I am currently based in Warwick, but willing to travel up to an hour for work and also have the
+                            facilities to work from home.
+                               
+                        <br />
+                            Please feel free to download a copy of my CV or to browse my Github repositories.
 
+                    </p>
+                    <a target="_blank"
+                        href="https://drive.google.com/file/d/1888M5C0p1VuKj7cIUJL5Ra9KQdV3dMHj/view?usp=sharing"
+                        className="btn btn-primary col-md-4">CV</a>
+                    <a target="_blank"
+                        href="https://github.com/oliviaowen1"
+                        className="btn btn-primary col-md-4">Github</a>
+                </div>
             </div>
 );
 }
-export default Jumbo;
+
+export default About;
