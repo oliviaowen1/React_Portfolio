356906c38d06783daa83afe68623f2183137ea19
Olivia Owen <olivia.owen@hotmail.com>
2021-05-16 19:23:27 +0100

    Updated app routes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
-import Jumbo from "./components/Jumbo";
-import About from "./components/About";
-import Cards from "./components/Cards"
 import Footer from "./components/Footer";
 
 function App() {
@@ -16,12 +13,11 @@ function App() {
       <Navbar />
     <Switch>
         <Route  exact path="/" component={Home} />
+        <Route  exact path="/home" component={Home} />
         <Route  exact path="/contact" component={Contact} />
         <Route  exact path="/portfolio" component={Portfolio} />
       </Switch>
-      <Jumbo />
-      <About />
-      <Cards />
+  
       <Footer />
       </div>
     </Router>
