a032bd899618bdffcea1cf96732222d89a763234
Olivia Owen <olivia.owen@hotmail.com>
2021-05-16 15:41:19 +0100

    Formatted document

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,15 +5,15 @@ function Footer() {
     const [isNavCollapsed, setIsNavCollapsed] = useState(true);
     const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
     return (
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark text-light" >
-<div class="container">
-            <h4 class="display-4">My Portfolio</h4>
-            <p class="lead">by Olivia Owen</p>
-            <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="./index.html">Home</a>
-                <a class="nav-link active" aria-current="page" href="./Portfolio.html">Portfolio</a>
-                <a class="nav-link active" aria-current="page" href="./contact.html">Contact</a>
-                <a href="https://www.linkedin.com/in/olivia-owen-37346aa4/" class="fa fa-linkedin"></a>
+<nav className="navbar navbar-expand-lg navbar-dark bg-dark text-light" >
+<div className="container">
+            <h4 className="display-4">My Portfolio</h4>
+            <p className="lead">by Olivia Owen</p>
+            <div className="navbar-nav">
+                <a className="nav-link active" aria-current="page" href="./index.html">Home</a>
+                <a className="nav-link active" aria-current="page" href="./Portfolio.html">Portfolio</a>
+                <a className="nav-link active" aria-current="page" href="./contact.html">Contact</a>
+                <a href="https://www.linkedin.com/in/olivia-owen-37346aa4/" className="fa fa-linkedin"></a>
                 
             </div>
             </div>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,21 +5,21 @@ function Navbar() {
     const [isNavCollapsed, setIsNavCollapsed] = useState(true);
     const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
     return (
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="./index.html">Olivia Owen</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div className="container-fluid">
+        <a className="navbar-brand" href="./index.html">Olivia Owen</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
             aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="./index.html">Home</a>
-                <a class="nav-link active" aria-current="page" href="./Portfolio.html">Portfolio</a>
-                <a class="nav-link active" aria-current="page" href="./contact.html">Contact</a>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+                <a className="nav-link active" aria-current="page" href="./index.html">Home</a>
+                <a className="nav-link active" aria-current="page" href="./Portfolio.html">Portfolio</a>
+                <a className="nav-link active" aria-current="page" href="./contact.html">Contact</a>
             </div>
         </div>
-        <a target="_blank" href="https://www.linkedin.com/in/olivia-owen-37346aa4/" class="fa fa-linkedin"></a>
+        <a target="_blank" href="https://www.linkedin.com/in/olivia-owen-37346aa4/" className="fa fa-linkedin"></a>
     </div>
 </nav>
 );
